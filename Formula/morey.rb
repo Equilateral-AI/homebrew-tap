ac44ff954335a3e35d35739cd1c51a58dd3ea352
James (Jim) Ford <100487721+JamesFordAI@users.noreply.github.com>
2026-09-19 23:33:02 +0000

    Update morey to 0.3.0 — all platforms

--- Formula/morey.rb
+++ Formula/morey.rb
@@ -7,22 +7,22 @@ class Morey < Formula
   on_macos do
     on_arm do
       url "https://github.com/Equilateral-AI/morey-releases/releases/download/v0.3.0/morey-0.3.0-darwin-arm64.tar.gz"
-      sha256 "75094e60bdb77eb01c00067de708367abe84bd4e8a6b677c1cf401985a29619e"
+      sha256 "96c7146e7e89c43b5a0a69972e1329f3d1311d4f68bab32e21dd0034959d1e86"
     end
     on_intel do
       url "https://github.com/Equilateral-AI/morey-releases/releases/download/v0.3.0/morey-0.3.0-darwin-x86_64.tar.gz"
-      sha256 "216658ea472123d7b02766e2a76c8089dc78b667abe01f12ef9ca4e7e3e66a47"
+      sha256 "15799affe51f3d46582b849aa709fa7fea460dd9652a7f232512194661b2b450"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/Equilateral-AI/morey-releases/releases/download/v0.3.0/morey-0.3.0-linux-arm64.tar.gz"
-      sha256 "e1922065d54b7c5359db830560bd090e6eba4abfbd2c01b350aac20eff0c230b"
+      sha256 "f3762177c6155950abbdc8b8c17266a1eb89058eefff776cd4cf3bd9e6bcdbca"
     end
     on_intel do
       url "https://github.com/Equilateral-AI/morey-releases/releases/download/v0.3.0/morey-0.3.0-linux-x86_64.tar.gz"
-      sha256 "14b5efb39529125723101b9e3e8dab1ff9d84833d235c09101b9041f0226539d"
+      sha256 "e09cc824f2583d3a63ca81b066809c7f1dbfa013763e7ce2ef2fc31fb8f3fd5c"
     end
   end
 
